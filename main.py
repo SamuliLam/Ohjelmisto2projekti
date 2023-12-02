ad0427f19176f32a032f4a93cd27cac6634256ab
Samuli Lamminmäki <samuli.lamminmaki@hotmail.com>
2023-12-02 16:29:16 +0200

    perkele

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 
 app.register_blueprint(endpoints.endpoints)
 
+@app.route('/')
+def index():
+    return render_template('index.jinja')
+
+@app.route('/difficulty')
+def difficulty():
+    return render_template('difficulty.jinja')
+
+@app.route('/game')
+def game():
+    return render_template('game.jinja')
+
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
 
-
-def index():
-    return render_template('index.html')
