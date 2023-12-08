aae288ac2b0c15e5367db34c4f8c3750178e611f
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2023-12-08 12:37:32 +0200

    muutokset

--- static/JavaScript/map.js
+++ static/JavaScript/map.js
@@ -1,11 +1,11 @@
 'use strict';
 
-async function sendAjaxRequest(difficulty){
+async function sendAjaxRequest(difficulty) {
     try {
         const response = await fetch(`http://127.0.0.1:5000/difficulty/${difficulty}`);
         const jsonData = await response.json();
         return Promise.resolve(jsonData)
-    }catch (err){
+    } catch (err) {
         console.log(err);
         return Promise.reject(err)
     }
@@ -15,17 +15,40 @@ const urlParams = new URLSearchParams(window.location.search);
 const difficulty = urlParams.get('difficulty');
 
 sendAjaxRequest(difficulty).then((response) => {
-    /*response tässä on json lista kaikista airporteista. Reponse muuttujaa voidaan käyttää markkereiden outputtaamiseen. */
+    /*response tässä on json lista kaikista airporteista. Reponsea voidaan käyttää markkereiden outputtaamiseen. */
     console.log(response);
     createMarkers(response);
 });
+/* last destination uses red marker */
+var redMarker = L.AwesomeMarkers.icon({
+    icon: 'plane',
+    markerColor: 'red'
+});
+var greenMarker = L.AwesomeMarkers.icon({
+    icon: 'plane',
+    markerColor: 'green'
+});
 
-function createMarkers(airports){
+let first_airport;
+let last_airport;
+
+function createMarkers(airports) {
     for (let i = 0; i < airports.length; i++) {
-      const airport = airports[i];
-      const marker = L.marker([airport[2], airport[3]]).addTo(mymap);
+
+        if (i === 0) {
+            first_airport = airports[i];
+            const first_marker = L.marker([first_airport[2], first_airport[3]], {icon: redMarker}).addTo(mymap);
+            console.log("first airport: " + first_airport);
+        }
+        if (i === airports.length - 1) {
+            last_airport = airports[i];
+            const last_marker = L.marker([last_airport[2], last_airport[3]], {icon: redMarker}).addTo(mymap);
+            console.log("last airport: " + last_airport);
+        }
+
+        const airport = airports[i];
+        const marker = L.marker([airport[2], airport[3]]).addTo(mymap);
     }
-    return;
 }
 
 let mymap;
