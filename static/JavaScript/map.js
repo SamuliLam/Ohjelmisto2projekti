9cc7d0d4a547c2df9c26b5c950215c98bbf91bdc
Samuli Lamminmäki <samuli.lamminmaki@hotmail.com>
2023-12-11 13:47:36 +0200

    airport names

--- static/JavaScript/map.js
+++ static/JavaScript/map.js
@@ -1,4 +1,5 @@
 'use strict';
+
 async function sendAjaxRequest(difficulty) {
     try {
         const response = await fetch(`http://127.0.0.1:5000/difficulty/${difficulty}`);
@@ -37,7 +38,11 @@ function createMarkers(airports) {
     startPoint.bindPopup(`<b>Start point</b><br>${airports[0][1]}`);
 
     for (let i = 1; i < airports.length; i++) {
-        const marker = L.marker([airports[i][2], airports[i][3]], {icon: greenMarker}).addTo(mymap);
+        const marker = L.marker([airports[i][2], airports[i][3]], {icon: greenMarker, airportName: airports[i][0]}).addTo(mymap);
+        const airport_name = marker.options.airportName;
+        //testaus alla airportin nimen saamiseksi
+        console.log(airport_name);
+        //testaus loppuu
         marker_list.push(marker);
 
         if (i === airports.length - 1) {
@@ -61,6 +66,7 @@ function createMarkers(airports) {
             if (clicked_markers.includes(marker) === false) {
                 marker.on('click', function (e) {
                     current_marker = marker;
+                    console.log("Clicked marker:", marker);
                     marker.setIcon(blueMarker);
                     clicked_markers.push(marker);
                     for (let j = 0; j < clicked_markers.length - 1; j++) {
@@ -78,11 +84,11 @@ function createMarkers(airports) {
                 });
             } else {
                 alert("You have already selected this destination.");
-                }
-
             }
 
         }
+
+    }
 
 }
 
@@ -124,77 +130,6 @@ let current_marker = null;
 //This list needs to be sent as json to backend. In python we can use json.loads() to convert it to a list and then use it to calculate the distance between the markers
 let clicked_markers = [];
 let marker_list = [];
-
-
-function createMarkers(airports) {
-    const startPoint = L.marker([airports[0][2], airports[0][3]], {icon: blueMarker}).addTo(mymap);
-    clicked_markers.push(startPoint);
-    const lastPoint = L.marker([airports[9][2], airports[9][3]], {icon: redMarker}).addTo(mymap);
-
-    startPoint.bindPopup(`<b>Start point</b><br>${airports[0][1]}`);
-
-    for (let i = 1; i < airports.length; i++) {
-        const marker = L.marker([airports[i][2], airports[i][3]], {icon: greenMarker}).addTo(mymap);
-        marker_list.push(marker);
-
-        if (i === airports.length - 1) {
-            marker.bindPopup(`<b>Destination</b><br>${airports[i][1]}`);
-            marker.setIcon(redMarker);
-            marker.on('click', function (e) {
-                // Check if all other markers have been clicked
-                if (clicked_markers.length === airports.length - 1) {
-                    current_marker = marker;
-                    marker.setIcon(blueMarker);
-                    // we use this list to calculate the distance between the markers
-                    clicked_markers.push(marker);
-                    for (let j = 0; j < clicked_markers.length - 1; j++) {
-                        clicked_markers[j].setIcon(greyMarker);
-                    }
-                } else {
-                    alert("Please click on all other destinations before selecting the final destination.");
-                }
-            });
-        } else {
-            marker.on('click', function (e) {
-                current_marker = marker;
-                marker.setIcon(blueMarker);
-                clicked_markers.push(marker);
-                for (let j = 0; j < clicked_markers.length - 1; j++) {
-                    clicked_markers[j].setIcon(greyMarker);
-                }
-                if (clicked_markers.length === marker_list.length) {
-                    const marker_cordiantes = clicked_markers.map(marker => marker.getLatLng());
-                    sendAjaxRequest2(marker_cordiantes).then((response) => {
-                        console.log("Received response:", response);
-                    });
-                }
-            });
-        }
-
-    }
-
-}
-
-// Here we send the list of markers to the backend
-async function sendAjaxRequest2(clicked_markers) {
-    try {
-        console.log("Sending data:", clicked_markers);  // Check if clicked_markers is not empty or undefined
-        const response = await fetch('http://127.0.0.1:5000/game/airport', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(clicked_markers)
-        });
-        const jsonData = await response.json();
-        console.log(jsonData);
-        return Promise.resolve(jsonData);
-    } catch (err) {
-        console.log(err);
-        return Promise.reject(err);
-    }
-}
-
 
 let mymap;
 
