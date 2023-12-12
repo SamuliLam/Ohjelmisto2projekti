c422ff2d5e77370f098d4f3e3746bf0f22f2a9cd
vjtimo <113420766+vjtimo@users.noreply.github.com>
2023-12-12 17:49:26 +0200

    Kilometrit viimeisen lentokentän sijaan

--- static/JavaScript/map.js
+++ static/JavaScript/map.js
@@ -147,7 +147,7 @@ function createMarkers(airports) {
                 sendAjaxRequest2(marker_cordiantes).then((response) => {
                     console.log("Received response:", response);
                     const distance = response.Response;
-                    document.getElementById('h2totaldistance').innerText = 'You traveled ' + distance + ' km';
+                    document.getElementById('h2content').innerText = 'You traveled ' + distance + ' km';
                     document.getElementById('rerun').innerText = 'Try again?';
 
                 });
